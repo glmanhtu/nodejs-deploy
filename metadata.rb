5d802062e0c31b1b519b13cb7b9cf5a288b8e656
Vu Manh Tu <tuvm@athena.studio>
2017-04-24 14:27:24 +0700

    Fix comma bug

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'glmanhtu@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures nodejs-deploy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 depends 'nginx'
 depends 'nodejs'
