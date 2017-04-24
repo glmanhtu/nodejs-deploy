121ad53f0a89cb06f0a86608b4e586a84b06ef36
Vu Manh Tu <tuvm@athena.studio>
2017-04-24 14:33:16 +0700

    Fix redirect too many times

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'glmanhtu@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures nodejs-deploy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 depends 'nginx'
 depends 'nodejs'
