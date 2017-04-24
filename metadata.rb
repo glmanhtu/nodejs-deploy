94e759e85174eb5626941599667bbc9239613fa6
Vu Manh Tu <tuvm@athena.studio>
2017-04-24 14:17:51 +0700

    Update version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'glmanhtu@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures nodejs-deploy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 depends 'nginx'
 depends 'nodejs'
